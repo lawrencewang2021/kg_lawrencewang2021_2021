fc1ae9f0f311e662df17e074f37698b4ece4683f
lawrencewang2021 <83440427+lawrencewang2021@users.noreply.github.com>
2021-04-30 12:15:49 +0800

    convert int to string

--- main.js
+++ main.js
@@ -10,3 +10,36 @@ if (arguments.length < 3) {
 for (var i = 2; i < arguments.length; i++) {
     dataList.push(arguments[i])
 }
+var resultList = [];
+for (var i = 0; i < dataList.length; i++) {
+    //console.log(dataList[i])
+    var intValue = dataList[i].toString();
+    var strValue = "";
+    //console.log(intValue.length);
+    for (var j = 0; j < intValue.length; j++) {
+        //console.log(intValue.charAt(j));
+        if (intValue.charAt(j) == '0')
+            strValue = strValue + "Zero";
+        else if (intValue.charAt(j) == '1')
+            strValue = strValue + "One";
+        else if (intValue.charAt(j) == '2')
+            strValue = strValue + "Two";
+        else if (intValue.charAt(j) == '3')
+            strValue = strValue + "Three";
+        else if (intValue.charAt(j) == '4')
+            strValue = strValue + "Four";
+        else if (intValue.charAt(j) == '5')
+            strValue = strValue + "Five";
+        else if (intValue.charAt(j) == '6')
+            strValue = strValue + "Six";
+        else if (intValue.charAt(j) == '7')
+            strValue = strValue + "Seven";
+        else if (intValue.charAt(j) == '8')
+            strValue = strValue + "Eight";
+        else if (intValue.charAt(j) == '9')
+            strValue = strValue + "Nine";
+        else
+            strValue = strValue + intValue.charAt(j);
+    }
+    resultList.push(strValue)
+}
